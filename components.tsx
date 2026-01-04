2cfaae37cf9044fcfa9dff3f2e666de709dd6149
symboleak <131503587+symboleak@users.noreply.github.com>
2026-01-04 11:33:34 +0100

    Add files via upload

---
+++ components.tsx
@@ -0,0 +1,5 @@
+// Ce fichier est déprécié.
+// Utilisez 'components-ui' pour les éléments visuels génériques.
+// Utilisez 'components-widgets' pour les composants métiers connectés aux données.
+export * from './components-ui';
+export * from './components-widgets';
