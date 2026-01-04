a951daa0a78b6e40ac613d454c549a858b36d721
symboleak <131503587+symboleak@users.noreply.github.com>
2026-01-04 11:36:49 +0100

    Delete components.tsx

--- components.tsx
+++
@@ -1,5 +0,0 @@
-// Ce fichier est déprécié.
-// Utilisez 'components-ui' pour les éléments visuels génériques.
-// Utilisez 'components-widgets' pour les composants métiers connectés aux données.
-export * from './components-ui';
-export * from './components-widgets';
